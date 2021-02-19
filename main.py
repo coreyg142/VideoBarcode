96374c3b53c60b588f086cd4139c36281aa0aeb8
Corey <coreyg142@gmail.com>
2021-02-19 17:00:05 -0500

    Reorganization, split up a massive function into two

--- main.py
+++ main.py
@@ -6,24 +6,10 @@ import numpy
 
 class BarcodeMaker:
     default_height = None
-    default_width = 1
 
-    def makeBarcode(self, source, nframes, blur, width=default_width, height=default_height):
-        video = cv2.VideoCapture(source)
-        if not video.isOpened():
-            raise FileNotFoundError("Video not found")
+    def makeBarcode(self, source, nFrames, blur, width, height=default_height):
 
-        totalFrames = video.get(cv2.CAP_PROP_FRAME_COUNT)
-        if not isinstance(nframes, int) or nframes < 1:
-            raise ValueError("Number of frames must be a positive integer")
-        elif nframes > totalFrames:
-            raise ValueError("Number of frames is larger than total available (" + str(totalFrames) + ")")
-        interval = totalFrames / nframes
-        success, image = video.read()
-        if not success:
-            raise IOError("Cannot read from video file")
-        if height is None:
-            height = image.shape[0]
+        video, interval, totalFrames, height = self.getVideoFile(source, nFrames, height)
 
         completedFrames = 0
         nextFrame = interval / 2
@@ -33,7 +19,7 @@ class BarcodeMaker:
         print("Starting visualization of \"" + source + "\" ")
 
         while True:
-            if completedFrames == nframes:
+            if completedFrames == nFrames:
                 break
 
             video.set(cv2.CAP_PROP_POS_FRAMES, int(nextFrame))
@@ -49,10 +35,10 @@ class BarcodeMaker:
 
             completedFrames += 1
             nextFrame += interval
-            endChar = '\r' if completedFrames != nframes else '\n'
+            endChar = '\r' if completedFrames != nFrames else '\n'
             timeElapsed = time.time() - startTime
             timeString = "\tTime Elapsed: {}".format(time.strftime("%H:%M:%S", time.gmtime(timeElapsed)))
-            print("Please wait ... " + str(completedFrames) + " out of " + str(nframes) + timeString, end=endChar)
+            print("Please wait ... " + str(completedFrames) + " out of " + str(nFrames) + timeString, end=endChar)
         video.release()
 
         if blur != 0:
@@ -61,6 +47,25 @@ class BarcodeMaker:
         print("Done.")
 
         return output
+
+    def getVideoFile(self, source, nFrames, height=default_height):
+        video = cv2.VideoCapture(source)
+        if not video.isOpened():
+            raise FileNotFoundError("Video not found")
+
+        totalFrames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+        if not isinstance(nFrames, int) or nFrames < 1:
+            raise ValueError("Number of frames must be a positive integer")
+        elif nFrames > totalFrames:
+            raise ValueError("Number of frames is larger than total available (" + str(totalFrames) + ")")
+        interval = totalFrames / nFrames
+        success, image = video.read()
+        if not success:
+            raise IOError("Cannot read from video file")
+        if height is None:
+            height = image.shape[0]
+
+        return video, interval, totalFrames, height
 
     def addBlur(self, image, amount):
         kernel_h = numpy.zeros((amount, amount))
@@ -79,7 +84,7 @@ def main():
 
     parser.add_argument("source", help="Video filename", type=str)
     parser.add_argument("dest", help="Filename for the final image", type=str)
-    parser.add_argument("-n", "--nframes", help="Number of frames in final visualization", type=int, required=True)
+    parser.add_argument("-n", "--nFrames", help="Number of frames in final visualization", type=int, required=True)
     parser.add_argument("-b", "--blur", help="Amount to blur if desired. Default 100", type=int, nargs='?', default=0,
                         const=100)
     parser.add_argument("-w", "--width", help="Specify the width to use, Default 1px", type=int, default=1)
@@ -87,7 +92,7 @@ def main():
     args = parser.parse_args()
 
     bm = BarcodeMaker()
-    output = bm.makeBarcode(args.source, args.nframes, args.blur, args.width)
+    output = bm.makeBarcode(args.source, args.nFrames, args.blur, args.width)
 
     cv2.imwrite(args.dest, output)
     print("Visualization saved to {}".format(args.dest))
